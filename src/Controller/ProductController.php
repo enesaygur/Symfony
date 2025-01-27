845258aa21bbafffe107b729b98d6aa44aea18c2
Enes <enesaygur387@gmail.com>
2025-01-28 01:04:29 +0300

    Product detail function and page created.

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -20,4 +21,26 @@ final class ProductController extends AbstractController
             'products' => $repository ->findAll(),
         ]);
     }
+
+
+    #[Route('/product/{id<\d+>}', name:'product_show')] // \d =[0-9] arası rakam, + ise  1 veya daha fazla basamaktan oluşmasıdır.
+    public function show(Product $product): Response
+    {
+        
+        return $this-> render('product/show.html.twig',[
+            'product'=> $product
+        ]);
+    }
+    // public function show($id, ProductRepository $repository): Response
+    // {
+    //     // dd($id);
+    //     // $product= $repository ->findOneBy(['id'=> $id]);
+    //     $product= $repository ->find($id);
+    //     if ($product == null) {
+    //         throw $this->createNotFoundException('Product not found');
+    //     }
+    //     return $this-> render('product/show.html.twig',[
+    //         'product'=> $product
+    //     ]);
+    // }
 }
