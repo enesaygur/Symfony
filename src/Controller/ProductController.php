c7f05e5ee046201f19918d55ee3c7e598a66459f
Enes <enesaygur387@gmail.com>
2025-01-27 22:44:25 +0300

    Fetch data in ProductController, display products on page

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,14 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+use App\Repository\ProductRepository;
 final class ProductController extends AbstractController
 {
     #[Route('/products', name: 'product_index')]
-    public function index(): Response
-    {
+    public function index(ProductRepository $repository): Response
+    {      
+        // $products =$repository ->findAll(); 1. yol
+        // dd($products); çıktıyı görmek için
+        // return $this->render('product/index.html.twig', [ 1. yol
+        //     'controller_name' => 'ProductController',
+        // ]);
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $repository ->findAll(),
         ]);
     }
 }
