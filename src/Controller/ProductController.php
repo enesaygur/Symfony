eb5620c318ed9dda594dfdd6537b63cf9d94a962
Enes <enesaygur387@gmail.com>
2025-01-28 19:53:50 +0300

    Added product creation functionality with a form.

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductRepository;
+use App\Form\ProductType;
+use Doctrine\ORM\EntityManagerInterface;
 final class ProductController extends AbstractController
 {
     #[Route('/products', name: 'product_index')]
@@ -32,15 +35,33 @@ final class ProductController extends AbstractController
         ]);
     }
     // public function show($id, ProductRepository $repository): Response
-    // {
-    //     // dd($id);
-    //     // $product= $repository ->findOneBy(['id'=> $id]);
-    //     $product= $repository ->find($id);
-    //     if ($product == null) {
-    //         throw $this->createNotFoundException('Product not found');
-    //     }
-    //     return $this-> render('product/show.html.twig',[
-    //         'product'=> $product
-    //     ]);
-    // }
+        // {
+        //     // dd($id);
+        //     // $product= $repository ->findOneBy(['id'=> $id]);
+        //     $product= $repository ->find($id);
+        //     if ($product == null) {
+        //         throw $this->createNotFoundException('Product not found');
+        //     }
+        //     return $this-> render('product/show.html.twig',[
+        //         'product'=> $product
+        //     ]);
+        // }
+
+    
+    #[Route('/product/new', name:'product_new')]
+    public function new(Request $request, EntityManagerInterface $manager):Response{
+        $product= new Product;
+        $form =$this->createForm(ProductType::class,$product);
+        $form-> handleRequest($request);
+        if($form->isSubmitted()){
+            $manager->persist($product);
+            $manager->flush();
+            // dd($request->request->all());
+            // dd($product);
+            return $this->redirectToRoute('product_show',['id'=>$product->getId(),]);
+        }
+        return $this->render('product/new.html.twig',[
+            'form'=> $form,
+        ]);
+    }
 }
