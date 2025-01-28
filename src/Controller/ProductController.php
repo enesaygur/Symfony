e9f9f4cdc5f032f63d36dc529861c914c6b576dd
Enes <enesaygur387@gmail.com>
2025-01-29 02:01:06 +0300

    Product update functionality implemented.

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -66,4 +66,20 @@ final class ProductController extends AbstractController
             'form'=> $form,
         ]);
     }
+    #[Route('/product/{id<\d+>}/edit', name:'product_edit')]
+    public function edit(Product $product, Request $request, EntityManagerInterface $manager): Response{
+        $form =$this->createForm(ProductType::class,$product);
+        $form-> handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->flush();
+            $this->addFlash('notice','Product updated successfully!');
+            // dd($request->request->all());
+            // dd($product);
+            return $this->redirectToRoute('product_show',['id'=>$product->getId(),]);
+        }
+        return $this->render('product/edit.html.twig',[
+            'form'=> $form,
+        ]);
+    }
 }
